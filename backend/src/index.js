f3d7b521017e05d45ba33eb000db9762be9b3acd
HerllonNovais <hnnascimento@gmail.com>
2025-05-01 20:57:53 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const electorsRoute = require('./routes/electors');
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/electors', electorsRoute);
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/eleitores', require('./routes/eleitores'));
 
