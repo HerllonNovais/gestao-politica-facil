e8ed42f16488cb23c1323b2565fae6f3d946f2d5
HerllonNovais <hnnascimento@gmail.com>
2025-05-01 20:42:21 -0300

    Create NotFound.jsx

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+const NotFound = () => (
+  <div className="flex items-center justify-center h-screen">
+    <h1 className="text-3xl font-bold">Página não encontrada</h1>
+  </div>
+);
+
+export default NotFound;
